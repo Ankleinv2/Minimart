635476f3adbaed7dcacdf4ed3de99f1760c675ea
Le Nhat Tung <lnt@gmail.com>
2023-06-10 11:37:34 +0700

    Edit config database

--- ManageMiniMart/DAL/Model1.Designer.cs
+++ ManageMiniMart/DAL/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\ACER\Desktop\PBL3\minimart\ManageMiniMart\DAL\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Admin\OneDrive - The University of Technology\Desktop\Minimart 96(4)\minimart\ManageMiniMart\DAL\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
